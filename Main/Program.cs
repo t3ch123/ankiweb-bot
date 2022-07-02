b9c625f7165d47cafefaa56acfa5d6941facb579
m.lyamets <m.lyamets@innopolis.ru>
2022-07-02 13:31:01 +0500

    Use appsettings

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,16 +1,26 @@
 ﻿using Anki.IL;
+using Microsoft.Extensions.Configuration;
+using TelegramAnki.Settings;
 
 namespace Main
 {
     class Program
     {
-        const string SECRET_TOKEN = "5339915288:AAFtdLwrYGHY7U6swyciwYUO5gODVG6FPwk";
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            Console.WriteLine("PgConnectionString {0}", settings.PgConnectionString);
+            Console.WriteLine("TgSecretToken {0}", settings.TgSecretToken);
+
             try
             {
                 System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
-                TelegaBot telegaBot = new(token: SECRET_TOKEN);
+                TelegaBot telegaBot = new(token: settings.TgSecretToken);
                 telegaBot.GetUpdates();
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
---
+++ TelegramAnki/Settings/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramAnki.Settings;
+
+public sealed class Settings
+{
+    public string PgConnectionString { get; set; } = null!;
+    public string TgSecretToken { get; set; } = null!;
+}
